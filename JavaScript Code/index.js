237bf6d586f3646b82519fb63417c13a4a2cf039
v4ri5 <tavarisct123@gmail.com>
2026-06-09 11:32:19 -0400

    Added Html Code from previous version, Cleaned up some of the integration and CSS code(Still cant fix that notebook picture AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH)

--- JavaScript Code/index.js
+++ JavaScript Code/index.js
@@ -2,15 +2,6 @@ function openPopup() {
     document.getElementById("athleteForm").style.display = "flex";
 }
 
-TL_button.addEventListener("click", function(){
-    window.open("/HTMLCode/TrainingLog.html");
-})
-
-const VV_button = document.getElementById("VB");
-
-VV_button.addEventListener("click", function(){
-    window.open("/HTMLCode/VideoVault.html");
-})
 function closePopup() {
     document.getElementById("athleteForm").style.display = "none";
 
